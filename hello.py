410559b7d98ceac1eb237b7684d292eac14b6bd4
Dhanvan3 <76443298+Dhanvan3@users.noreply.github.com>
2020-12-23 09:54:31 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,10 @@
+print('Hello World')
+print('What is your name?')
+myName=input()
+print('Its nice to meet you ' +myName)
+print('The length of your name is:')
+print(len(myName))
+print('what is your age?')
+myAge=input()
+print('You will be '+ str(int(myAge)+1) + 'in a year.')
+
